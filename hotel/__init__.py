d1d0bcbfbb4432849aa342ff110301641cf30da6
raamsaranyak <k.raamsaranya@gmail.com>
2026-07-05 21:31:20 +0530

    Prepare app configuration for deployment

--- hotel/__init__.py
+++ hotel/__init__.py
@@ -3,10 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_migrate import Migrate
+import os
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hotel.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key="Ap12345@"
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "Ap12345@")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL","sqlite:///hotel.db")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 bcrypt=Bcrypt(app)
