dcd1947ec23ff5b49fdd2b0e10990a5ef2b69407
raamsaranyak <k.raamsaranya@gmail.com>
2026-07-05 21:50:50 +0530

    Debug database creation

--- run.py
+++ run.py
@@ -2,9 +2,11 @@ from hotel import app, db
 from hotel.models import *
 
 with app.app_context():
-    print("Creating database tables...")
+    print("=" * 50)
+    print("Creating database...")
     db.create_all()
-    print("Database tables created.")
+    print("Database created successfully!")
+    print("=" * 50)
 
 if __name__ == "__main__":
     app.run()
