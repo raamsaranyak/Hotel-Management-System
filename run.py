3a64a9fde3ff7e0087b8ba767ef48f2d8bdb2d24
raamsaranyak <k.raamsaranya@gmail.com>
2026-07-14 15:54:53 +0530

    Add demo database

--- run.py
+++ run.py
@@ -2,11 +2,7 @@ from hotel import app, db
 from hotel.models import *
 
 with app.app_context():
-    print("=" * 50)
-    print("Creating database...")
     db.create_all()
-    print("Database created successfully!")
-    print("=" * 50)
 
 if __name__ == "__main__":
     app.run()
